d0517674cc5ff9b7f3813812150962c754b17835
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-03-15 10:49:02 -0500

    Paso 1. Falla para obtener la cantidad de numeros de un string vacio

---
+++ NumbersOperations.py
@@ -0,0 +1,10 @@
+
+
+class NumbersOperations:
+
+    def elementsNumbers(self, numbers):
+        arraynumbers = numbers.split(',')
+        if arraynumbers[0] == '':
+            return [0]
+        array = [len(arraynumbers)]
+        return array
---
+++ NumbersOperationsTest.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from NumbersOperations import NumbersOperations
+
+class NumbersOperationsTest(TestCase):
+
+    numbersOperations = NumbersOperations()
+
+    def test_elementsNumbers(self):
+        self.assertEquals(self.numbersOperations.elementsNumbers('')[0],1,"Elements number")
