bd64b96c3c668a53502ca87fbbd1fd5ad08dac87
Abimelec Cuesta <acuestap@gmail.com>
2016-03-15 12:21:40 -0500

    Paso 4 - Se modifica para que pase la prueba de obtener el numero de elementos, el minimo, el maximo y el promedio cuando se envia una cadena con dos números (Verde)

--- NumbersOperationsTest.py
+++ NumbersOperationsTest.py
@@ -52,4 +52,4 @@ class NumbersOperationsTest(TestCase):
         self.assertEquals(self.numbersOperations.elementsAvg('3'),[1,3,3,3],"Elements number, minimum, maximum, and average")
 
     def test_elementsAvgTwoNumbers(self):
-        self.assertEquals(self.numbersOperations.elementsAvg('4,2'),[1,3,3,3],"Elements number, minimum, maximum, and average")
+        self.assertEquals(self.numbersOperations.elementsAvg('4,2'),[2,2,4,3],"Elements number, minimum, maximum, and average")
