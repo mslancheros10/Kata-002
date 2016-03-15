fe0f2721418dfb250e02f56ab9404f42da662fd3
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-03-15 12:08:46 -0500

    Refactor NumbersOperations

--- NumbersOperations.py
+++ NumbersOperations.py
@@ -6,6 +6,7 @@ class NumbersOperations:
         arraynumbers = numbers.split(',')
         if arraynumbers[0] == '':
             return [0]
+        arraynumbers = map(int,arraynumbers)
         array = [len(arraynumbers)]
         return array
 
@@ -15,6 +16,7 @@ class NumbersOperations:
         arraynumbers = numbers.split(',')
         if arraynumbers[0] == '':
             return [elementNumers[0],0]
+        arraynumbers = map(int,arraynumbers)
         array = [min(arraynumbers)]
         return [elementNumers[0],int(array[0])]
 
@@ -23,6 +25,15 @@ class NumbersOperations:
         arraynumbers = numbers.split(',')
         if arraynumbers[0] == '':
             return [minNumber[0],minNumber[1],0]
+        arraynumbers = map(int,arraynumbers)
         array = [max(arraynumbers)]
         return [minNumber[0],minNumber[1],int(array[0])]
+
+    def elementsAvg(self, numbers):
+        maxNumber = self.elementsMax(numbers)
+        arraynumbers = numbers.split(',')
+        if arraynumbers[0] == '':
+            return [maxNumber[0],maxNumber[1],0]
+        array = [max(arraynumbers)]
+        return [maxNumber[0],maxNumber[1],int(array[0])]
 
